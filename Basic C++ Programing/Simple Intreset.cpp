4697097cbef67aa0f29975f3817fad8eddb0781b
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-23 09:38:23 -0800

    Add files via upload

---
+++ Basic C++ Programing/Simple Intreset.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float principle=23;
+	float rate=78;
+	float time=3;
+	float SI=principle*rate*time/100;
+	cout<<"Simple Intrest = ";
+	cout<<SI;
+}
