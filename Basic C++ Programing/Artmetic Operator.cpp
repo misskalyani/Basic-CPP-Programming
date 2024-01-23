af78252ace60e0ec34f299151850398271469438
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-23 09:36:16 -0800

    Add files via upload

---
+++ Basic C++ Programing/Artmetic Operator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//	int x=6,y=7;
+//	cout<<x+y<<endl;//13
+//	cout<<x-y<<endl;//-1
+//	cout<<x*y<<endl;//42
+//	cout<<x/y<<endl;//0
+//	
+//	
+	
+	float x=6,y=7;
+	cout<<x+y<<endl;//13
+	cout<<x-y<<endl;//-1
+	cout<<x*y<<endl;//42
+	cout<<x/y<<endl;//0.857143
+}
