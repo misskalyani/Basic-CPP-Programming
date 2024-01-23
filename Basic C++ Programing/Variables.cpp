729d8cbff8704fd3ab83ca1162477d73d137cbfe
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-23 09:39:28 -0800

    Add files via upload

---
+++ Basic C++ Programing/Variables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;//decleration
+	x=5;//initialization
+	y=9;
+	cout<<x;
+	cout<<"\n";//newline
+	cout<<x+3;
+	cout<<"\n";
+	cout<<x*y;
+}
