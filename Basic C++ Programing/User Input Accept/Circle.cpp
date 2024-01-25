f8a1384b2230cd60dd2e4e15403950701f824db3
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-25 06:02:44 -0800

    Add files via upload

---
+++ Basic C++ Programing/User Input Accept/Circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r;
+	cout<<"Enter Radius::";
+	cin>>r;
+	cout<<"Area Of Circle is : "<<3.14*r*r;
+	cout<<"\n Circumference : "<<2*3.14*r;
+}
