a3e0d8c7a76da6a3f30a15a847dac808a287d03c
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-25 06:14:42 -0800

    Add files via upload

---
+++ Basic C++ Programing/User Input Accept/Sum of num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter First Number :";
+	cin>>a;
+	cout<<"Enter second Number :";
+	cin>>b;
+	cout<<"Enter third Number :";
+	cin>>c;
+	cout<<"Sum is : "<<a+b+c;
+}
