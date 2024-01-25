842f17b813bf3f07521e1ab92d9498f64abe56ad
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-25 06:12:12 -0800

    Add files via upload

---
+++ Basic C++ Programing/User Input Accept/Rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int l,b;
+	cout<<"Enter length::";
+	cin>>l;
+	cout<<"Enter breadth::";
+	cin>>b;
+	cout<<"Area Of Rectangle is : "<<l*b;
+	cout<<"\nPerimeter : "<<2*(l+b);
+}
