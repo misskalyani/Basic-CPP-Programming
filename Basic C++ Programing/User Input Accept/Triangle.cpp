2a69a22abe863425cceee25b1d8d71430e24ed3f
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-25 06:15:34 -0800

    Add files via upload

---
+++ Basic C++ Programing/User Input Accept/Triangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float h,b;
+	cout<<"Enter height::";
+	cin>>h;
+	cout<<"Enter base::";
+	cin>>b;
+	cout<<"Area Of Triangle is : "<<0.5*b*h;
+	cout<<"\nPerimeter : "<<3*(b*h);
+}
