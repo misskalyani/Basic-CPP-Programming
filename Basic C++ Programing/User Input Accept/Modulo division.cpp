e5437465d27dc0a808a62255b6f7d8d036505651
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-25 06:08:05 -0800

    Add files via upload

---
+++ Basic C++ Programing/User Input Accept/Modulo division.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter First Number :";
+	cin>>a;
+	cout<<"Enter second Number :";
+	cin>>b;
+	cout<<"Modulo Division  is : "<<a%b;//print remainder
+}
