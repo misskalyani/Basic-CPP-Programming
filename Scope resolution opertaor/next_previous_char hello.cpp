9d652b6b7b2d022f6dcadb856e6f0c4df9fd16a5
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-26 17:29:20 +0530

    Add files via upload

---
+++ Scope resolution opertaor/next_previous_char hello.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter Value : ";
+    cin>>i;
+    cout<<i*i<<"\n";
+    
+    
+    
+    
+    char ch;
+    cout<<"\nEnter Character : ";
+    cin>>ch;
+
+    char y = (char)ch+1;
+    cout<<"\n Next Character is : ";   
+    cout<<y<<"\n";
+   
+}
