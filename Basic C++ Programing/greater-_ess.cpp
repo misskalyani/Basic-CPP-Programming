1a68de0c2b5264613642ef3c6c387a9a1e424144
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-30 08:14:31 +0530

    Add files via upload

---
+++ Basic C++ Programing/greater-_ess.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter Number : ";
+	cin>>n;
+	if(n>1000)
+	{
+		cout<<"Number is Greater than 1000";
+	}
+    else
+	{
+		cout<<"Number is less than 1000";
+	}
+}
