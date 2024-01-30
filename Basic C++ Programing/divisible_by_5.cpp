70da6ee0e5e94e45ad291e435a2e79b64595c74a
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-30 08:13:18 +0530

    Add files via upload

---
+++ Basic C++ Programing/divisible_by_5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter Number : ";
+	cin>>n;
+	if(n%5==0)
+	{
+		cout<<"Number is divisible by 5";
+	}
+    else
+	{
+		cout<<"Number is NOT divisible by 5";
+	}
+}
