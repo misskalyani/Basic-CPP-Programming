338029e5f38001a3cc2e90c50493bacc8b5b3ed1
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-30 08:17:31 +0530

    Add files via upload

---
+++ Basic C++ Programing/Postive_Negative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter Number : ";
+	cin>>n;
+	if(n>0)
+	{
+		cout<<"Number is Positive";
+	}
+    else
+	{
+		cout<<"Number is Negative";
+	}
+}
