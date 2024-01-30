70d293c27b45372f5f787c282236f56fafb09291
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-30 16:06:15 +0530

    Add files via upload

---
+++ Basic C++ Programing/acceptdatprint month.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter Day : ";
+	cin>>a;
+	if(a==1)
+	{
+		cout<<"JANUARY";
+	}
+	else if(a==2)
+	{
+		cout<<"FEBRUARY";
+	}
+	else if(a==3)
+	{
+		cout<<"MARCH";
+	}
+	else if(a==4)
+	{
+		cout<<"APRIL";
+	}
+	else if(a==5)
+	{
+		cout<<"MAY";
+	}
+	else if(a==6)
+	{
+		cout<<"JUNE";
+	}
+	else if(a==7)
+	{
+		cout<<"JULY";
+	}
+	else if(a==8)
+	{
+		cout<<"AUGUST";
+	}
+	else if(a==9)
+	{
+		cout<<"SEPTMEBER";
+	}
+	else if(a==10)
+	{
+		cout<<"OCTOMBER";
+	}
+	else if(a==11)
+	{
+		cout<<"NOVEMBER";
+	}
+	else if(a==12)
+	{
+		cout<<"NOVEMBER";
+	}
+	else
+	{
+		cout<<"Invalid Choice";
+	}
+}
