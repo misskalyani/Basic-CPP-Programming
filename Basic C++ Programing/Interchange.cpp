a78d4b00a0ec222e48137eacc9bea2a61d116f87
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-30 08:14:55 +0530

    Add files via upload

---
+++ Basic C++ Programing/Interchange.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	#include<iostream>
+	cout<<"Enter Number a : ";
+	cin>>a;
+	cout<<"Enter Number b : ";
+	cin>>b;
+	
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	cout<<"First Number : "<<a;
+	cout<<"\nSecond Number : "<<b;
+}
+
