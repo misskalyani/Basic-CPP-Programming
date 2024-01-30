8ab7fe924409d469970ff271facaac1aabd06f65
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-30 14:52:28 +0530

    Add files via upload

---
+++ Basic C++ Programing/Aritmetic_harmonic_mean.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,am,hm;
+	#include<iostream>
+	cout<<"Enter Number a : ";
+	cin>>a;
+	cout<<"Enter Number b : ";
+	cin>>b;
+	
+	am=(a+b)/2;
+	hm=(a*b)/(a+b);
+	cout<<"Aritmetic Mean : "<<am;
+	cout<<"\nHarmonic Mean : "<<hm;
+}
+
