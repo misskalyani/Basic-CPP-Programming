2f3ff570138c4add9ae9b882929669afb9de0394
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-30 08:13:45 +0530

    Add files via upload

---
+++ Basic C++ Programing/Even_odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter Number : ";
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<"Number is Even";
+	}
+    else
+	{
+		cout<<"Number is ODD";
+	}
+}
