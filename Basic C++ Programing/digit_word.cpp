73348bbacfe8e7b266c7c2a54195725c357e076b
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-01 08:38:08 +0530

    Add files via upload

---
+++ Basic C++ Programing/digit_word.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter Digit : ";
+	cin>>a;
+	if(a==0)
+	{
+		cout<<"ZERO";
+	}
+	else if(a==1)
+	{
+		cout<<"ONE";
+	}
+	else if(a==2)
+	{
+		cout<<"TWO";
+	}
+	else if(a==3)
+	{
+		cout<<"THREE";
+	}
+	else if(a==4)
+	{
+		cout<<"FOUR";
+	}
+	else if(a==5)
+	{
+		cout<<"FIVE";
+	}
+	else if(a==6)
+	{
+		cout<<"SIX";
+	}
+	else if(a==7)
+	{
+		cout<<"SEVEN";
+	}
+	else if(a==8)
+	{
+		cout<<"EIGHT";
+	}
+	else if(a==9)
+	{
+		cout<<"NINE";
+	}
+	else
+	{
+		cout<<"Invalid Case";
+	}
+}
