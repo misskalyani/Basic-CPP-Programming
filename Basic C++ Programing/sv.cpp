d29c9e2a39cbee8752e7bbcb4cb80629b79049df
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-02 08:14:18 +0530

    Add files via upload

---
+++ Basic C++ Programing/sv.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float u,a,t,v,s;
+	cout<<"Enter u,a and t :: ";
+	cin>> u >> a >> t;
+	v=u+a*t;
+	cout<<"v = "<<v;
+	s=u+a*t;
+	cout<<"\ns = "<<s;
+}
