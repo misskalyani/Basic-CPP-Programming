69af5ac42de4fd2a903a628cf22101c0ddcf46dd
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-02 21:01:00 +0530

    Add files via upload

---
+++ Basic C++ Programing/Three_Digit_num.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int ch;
+	cout<<"Enter Nmber : ";
+	cin>>ch;
+    if(ch>99 && ch<1000)
+    {
+    	cout<<"Three Digit Number ";
+	}
+	else
+	{
+			cout<<" NOT Three Digit Number ";
+	}
+}
