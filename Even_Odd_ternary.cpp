a28be1e0eccef19fa817af024497c937f4f3157b
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-04 20:24:21 +0530

    Add files via upload

---
+++ Even_Odd_ternary.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter Number: ";
+	cin>>n;
+	//(condition)    ?   if true:    if false:
+	(n%2==0) ? cout<<"Even Number ":  cout<<"Odd Number "; 
+}
