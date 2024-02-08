6ed6723372cc4dea794dd7d817a768aee1acc27e
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-08 15:24:52 +0530

    Add files via upload

---
+++ Pattern/Square/Alphabte_same_Square.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r,i,j;
+	cout<<"Enter number of rows : ";
+	cin>>r;
+	for(i=1;i<=r;i++)
+	{
+		for(j=1;j<=r;j++)
+		{
+		   cout<<(char)(i+64) <<"  ";//typecast
+		}
+		cout<<"\n";
+	}
+}
