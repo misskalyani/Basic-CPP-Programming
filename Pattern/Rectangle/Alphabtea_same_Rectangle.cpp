3a462bd87ab2e400957a75b74b9572431da8d6f0
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-08 15:08:28 +0530

    Add files via upload

---
+++ Pattern/Rectangle/Alphabtea_same_Rectangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r,c,i,j;
+	cout<<"Enter number of rows : ";
+	cin>>r;
+	cout<<"Enter number of columns : ";
+	cin>>c;
+	for(i=1;i<=r;i++)
+	{
+		for(j=1;j<=c;j++)
+		{
+		   cout<<(char)(i+64)<<" ";//typecasting
+		}
+		cout<<"\n";
+	}
+}
