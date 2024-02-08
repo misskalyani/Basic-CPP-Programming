c266e5099af27896d549fd2fb6d66876276a48d9
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-08 15:32:21 +0530

    Add files via upload

---
+++ Math/Factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,product=1,i=1;
+	cout<<"Enter Limit :: ";
+	cin>>n;
+    while(i<=n)
+	{
+		
+		product=product*i;
+	    i++;
+	}
+	cout<<"Factorial = "<<product;
+}
