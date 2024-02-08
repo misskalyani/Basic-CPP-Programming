10b130d6c404b308ec2b30692321d4ea2a741eaa
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-08 15:33:53 +0530

    Add files via upload

---
+++ Math/product_digit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,product=1,ld;
+	cout<<"Enter Limit :: ";
+	cin>>n;
+	while(n!=0)
+	{
+		ld=n%10;
+		n=n/10;
+		product=product*ld;
+	}
+	cout<<"Product of digit = "<<product;
+}
