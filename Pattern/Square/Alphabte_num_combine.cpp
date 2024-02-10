eb4212949becb695ac35133f348666ec2cb676a5
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-10 17:15:01 +0530

    Add files via upload

---
+++ Pattern/Square/Alphabte_num_combine.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n;
+	cout<<"Enter Limit : ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i%2==0)
+			cout<<j<<" ";
+			else
+			cout<<(char )(j+64)<<" ";
+			
+		}
+		cout<<"\n";
+	}
+}
