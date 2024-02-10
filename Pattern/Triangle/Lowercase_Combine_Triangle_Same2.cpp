bd38976f4339e17c527e8fa59ee7d628a159ffbf
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-10 17:25:44 +0530

    Add files via upload

---
+++ Pattern/Triangle/Lowercase_Combine_Triangle_Same2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n;
+	cout<<"Enter Limit : ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			if(i%2==0)
+		      cout<<i<<" ";
+			else
+				cout<<(char )(i+96)<<" ";
+			
+		}
+		cout<<"\n";
+	}
+}
