66948a3c80b02ce6446f3e9bf92a3107baebc61b
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-10 17:24:21 +0530

    Add files via upload

---
+++ Pattern/Triangle/Same_Alphabate_traingle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n;
+	cout<<"Enter Limit : ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			cout<<(char )(i+64)<<" ";
+		}
+		cout<<"\n";
+	}
+}
