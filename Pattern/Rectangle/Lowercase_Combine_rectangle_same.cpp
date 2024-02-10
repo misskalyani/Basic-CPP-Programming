487b4b2a59210b9736b0f8b9856252c423337e56
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-10 17:11:39 +0530

    Add files via upload

---
+++ Pattern/Rectangle/Lowercase_Combine_rectangle_same.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n1,n2;
+	cout<<"Enter Number Of Row : ";
+	cin>>n1;
+	cout<<"Enter Number Of Column : ";
+	cin>>n2;
+	for(i=1;i<=n1;i++)
+	{
+		for(j=1;j<=n2;j++)
+		{
+			if(i%2==0)
+			cout<<i<<" ";
+			else
+				cout<<(char )(i+96)<<" ";
+		
+		}
+		cout<<"\n";
+	}
+}
