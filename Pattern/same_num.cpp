94bbf9990dea7c966eb8dfe7b514cf3b07c5db5b
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-12 19:46:34 +0530

    Add files via upload

---
+++ Pattern/same_num.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n;
+	cout<<"Enter Limit : ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=i+1;j<=n-i+1;j++)
+		{
+		
+			cout<<i<<" ";
+		}
+		cout<<"\n";
+	}
+}
